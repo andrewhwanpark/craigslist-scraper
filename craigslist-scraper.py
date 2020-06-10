60e35543a542bed4b9dcf986afa521c45063ff32
Andrew Park <hp1141@stern.nyu.edu>
2020-06-10 15:11:00 -0400

    Added url & title scraping

--- craigslist-scraper.py
+++ craigslist-scraper.py
@@ -16,7 +16,7 @@ class CraigslistScraper(object):
         # URL
         self.url = f"https://{location}.craigslist.org/search/sss?search_distance={radius}&postal={postal}&max_price={max_price}"
         # Selenium browser
-        self.driver = webdriver.Safari()
+        self.driver = webdriver.Chrome()
         # Load delay
         self.delay = 3
 
@@ -25,10 +25,30 @@ class CraigslistScraper(object):
         try:
             wait = WebDriverWait(self.driver, self.delay)
             # Wait until HTML element "searchform" is loaded
-            wait.until(EC.presence_of_element_located(By.ID, "searchform"))
+            wait.until(EC.presence_of_element_located((By.ID, "searchform")))
             print("Ready")
         except TimeoutException:
             print("Loading timed out")
+
+    # Extract Post titles from all class "result-row" elements
+    def extract_post_titles(self):
+        all_post = self.driver.find_elements_by_class_name("result-row")
+        post_title_list = []
+
+        for post in all_post:
+            post_title_list.append(post.text)
+
+        return post_title_list
+
+    def extract_post_urls(self):
+        all_url = []
+        html_page = urllib.request.urlopen(self.url)
+        soup = BeautifulSoup(html_page, "lxml")
+
+        for link in soup.findAll("a", {"class": "result-title hdrlnk"}):
+            all_url.append(link["href"])
+        print(all_url)
+        return all_url
 
 location = "newyork"
 postal = "10012"
@@ -37,3 +57,5 @@ radius = "5"
 
 scraper = CraigslistScraper(location, postal, max_price, radius)
 scraper.load_craigslist_url()
+# scraper.extract_post_titles()
+scraper.extract_post_urls()
